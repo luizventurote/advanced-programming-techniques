b6d57810fec8a93fc5f8dfbc854474630cd7704f
luizventurote <luiz_17@ymail.com>
2014-10-29 09:48:45 -0200

    Prim algorithm modified

--- graph/C/prim.h
+++ graph/C/prim.h
@@ -1,5 +1,6 @@
 // Number of vertices in the graph
 #define PRIM_EDGES_QTY 7 
+#define PRIM_MAX_VALUE 999
  
  
  /**
@@ -14,7 +15,7 @@ void PrimMST() {
 	// Init graph
 	Prim_initGraph(graph);
    
-	// Add adges in graph 1
+	// Add adges in graph
 	Prim_addEdge(graph, 0, 1, 5);
 	Prim_addEdge(graph, 0, 2, 3);
 	Prim_addEdge(graph, 1, 0, 5);
@@ -53,16 +54,14 @@ void PrimMST() {
 int minKey(int key[], int mstSet[]) {
 	
    // Initialize min value
-   int min = INT_MAX, min_index;
-   
-   int v=0;
+   int min=PRIM_MAX_VALUE, min_index=0, v=0;
  
-	for (v=0; v<PRIM_EDGES_QTY; v++) {
+	for(v=0; v<PRIM_EDGES_QTY; v++) {
 		
-		if (mstSet[v] == 0 && key[v] < min) {
-			min = key[v], min_index = v;	
-		}
-		
+		if (mstSet[v]==0 && key[v]<min) {
+			min 	  = key[v];
+			min_index = v;	
+		}	
 	}
 
    return min_index;
@@ -83,7 +82,7 @@ int printMST(int parent[], int n, int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY]) {
 
 	printf("Edge   Weight\n");
 	
-	for (i = 1; i < PRIM_EDGES_QTY; i++) {
+	for (i=1; i<PRIM_EDGES_QTY; i++) {
 		printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
 	}
       
@@ -96,53 +95,48 @@ int printMST(int parent[], int n, int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY]) {
  */
 void Prim(int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY]) {
 	
-	int parent[PRIM_EDGES_QTY]; 	// Array to store constructed MST
-	int key[PRIM_EDGES_QTY];   	// Key values used to pick minimum weight edge in cut
-	int mstSet[PRIM_EDGES_QTY];  	// To represent set of vertices not yet included in MST
+	int i=0, n=PRIM_EDGES_QTY, count = 0, u=0, v=0;
+	
+	int parent[n]; 	// Array to store constructed MST
+	int key[n];   	// Key values used to pick minimum weight edge in cut
+	int mstSet[n];  // To represent set of vertices not yet included in MST
  
 	// Initialize all keys as INFINITE
-	int i = 0;
-     
-	for (i=0; i<PRIM_EDGES_QTY; i++) {
-		parent[i] = 0;
-		key[i] = INT_MAX;
+	for (i=0; i<n; i++) {
+		parent[i] = 0;		
 		mstSet[i] = 0;
+		key[i] 	  = PRIM_MAX_VALUE;
 	}
-	// Always include first 1st vertex in MST.
-	key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
-	parent[0] = -1; // First node is always root of MST 
- 
-	// The MST will have V vertices
-	int count = 0;
 	
-	for(count=0; count<PRIM_EDGES_QTY-1; count++) {
+	// Include first vertex in MST
+	key[0] 	  = 0;
+	parent[0] = -1;
+	
+	for(count=0; count<n-1; count++) {
 		
-		// Pick thd minimum key vertex from the set of vertices not yet included in MST
-        int u = minKey(key, mstSet);
+		// Pick the minimum key vertex from the set of vertices not yet included in MST
+        u = minKey(key, mstSet);
  
         // Add the picked vertex to the MST Set
         mstSet[u] = 1;
  
         // Update key value and parent index of the adjacent vertices of the picked vertex. Consider only those vertices which are not yet included in MST
-        int v=0;
-		for (v=0; v<PRIM_EDGES_QTY; v++) {
+		for(v=0; v<n; v++) {
 			
 			// graph[u][v] is non zero only for adjacent vertices of m
 			// mstSet[v] is false for vertices not yet included in MST
 			// Update the key only if graph[u][v] is smaller than key[v]
-			if (graph[u][v] && mstSet[v] == 0 && graph[u][v]<key[v]) {
+			if(graph[u][v] && mstSet[v]==0 && graph[u][v]<key[v]) {
 				
-				parent[v]  = u;
-				key[v] = graph[u][v];
+				parent[v] = u;
+				key[v] 	  = graph[u][v];
 			
 			}
-			
 		}
-		
      }
  
      // print the constructed MST
-     printMST(parent, PRIM_EDGES_QTY, graph);
+     printMST(parent, n, graph);
 }
 
 
@@ -168,18 +162,16 @@ void Prim_addEdge(int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY], int src, int dest, 
  */
 void Prim_printGraph(int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY]) {
 	
-	int edges_qty= PRIM_EDGES_QTY;
-	
-	int i=0, j=0;
+	int n=PRIM_EDGES_QTY, i=0, j=0;
 	
 	printf("\n");
 	
-	for(i=0; i<edges_qty; ++i) {
+	for(i=0; i<n; ++i) {
 		
 		// Graph index
 		if(i==0) {
 			
-			for(j=0; j<edges_qty; ++j) {
+			for(j=0; j<n; ++j) {
 				
 				if(j==0)
 				printf("   | ");
@@ -190,13 +182,13 @@ void Prim_printGraph(int graph[PRIM_EDGES_QTY][PRIM_EDGES_QTY]) {
 							
 			printf("\n");	
 			
-			for(j=0; j<edges_qty; ++j)
+			for(j=0; j<n; ++j)
 				printf("------", j);
 				
 			printf("\n");	
 		}
 		
-		for(j=0; j<edges_qty; ++j) {
+		for(j=0; j<n; ++j) {
 			
 			
 			if(j==0)
