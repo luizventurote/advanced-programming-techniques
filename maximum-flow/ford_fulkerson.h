d0a25adf7563209829e96ca8681147c828f1f16d
luizventurote <luiz_17@ymail.com>
2014-11-07 01:15:40 -0200

    Ford Fulkerson modified

--- maximum-flow/ford_fulkerson.h
+++ maximum-flow/ford_fulkerson.h
@@ -1,6 +1,6 @@
 // Number of vertices in the graph
 // Número de vértices do grafo
-#define FF_VERTEX_QTY 5
+#define FF_VERTEX_QTY 6
 
 // Maximum value used in the Kruskal algorithm
 // Valor máximo utilizado no algoritmo de Kruskal
@@ -9,17 +9,12 @@
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
-#define MAX_NODES 1000
-#define oo 1000000000
 
-int n;  // number of nodes
-int e;  // number of edges
-int capacity[MAX_NODES][MAX_NODES]; // capacity matrix
-int flow[MAX_NODES][MAX_NODES];     // flow matrix
-int color[MAX_NODES]; // needed for breadth-first search               
-int pred[MAX_NODES];  // array to store augmenting path
+int capacity[FF_VERTEX_QTY][FF_VERTEX_QTY]; // capacity matrix
+int color[FF_VERTEX_QTY]; // needed for breadth-first search               
+int pred[FF_VERTEX_QTY];  // array to store augmenting path
 int head,tail;
-int q[MAX_NODES+2];
+int q[FF_VERTEX_QTY+2];
 
 
 /**
@@ -36,12 +31,16 @@ void FordFulkersonMF() {
 	
 	// Add adges in graph
 	// Adiciona arestas no grafo
-	FF_addEdge(graph, 0, 1, 1);
-	FF_addEdge(graph, 1, 2, 2);
-	FF_addEdge(graph, 2, 3, 8);
-	FF_addEdge(graph, 2, 4, 2);
-	FF_addEdge(graph, 3, 0, 5);
-	FF_addEdge(graph, 4, 1, 1);
+	FF_addEdge(graph, 0, 1, 16);
+	FF_addEdge(graph, 0, 2, 13);
+	FF_addEdge(graph, 1, 2, 10);
+	FF_addEdge(graph, 1, 3, 12);
+	FF_addEdge(graph, 2, 1, 4);
+	FF_addEdge(graph, 2, 4, 14);
+	FF_addEdge(graph, 3, 2, 9);
+	FF_addEdge(graph, 4, 3, 7);
+	FF_addEdge(graph, 3, 5, 20);
+	FF_addEdge(graph, 4, 5, 4);
 	
 	// Print graph
 	// Exibe o grafo
@@ -49,20 +48,7 @@ void FordFulkersonMF() {
 	
     // Print the result
     // Exibe o resultado
-    FordFulkerson(graph);
-    
-}
-
-
-/**
- * Ford Fulkerson algorithm
- * @author Luiz Venturote
- */
-void FordFulkerson(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY]) {
-	
-	read_input_file();
-    
-    printf("%d\n",max_flow(0,n-1));
+    printf(" %d \n\n", FordFulkerson(graph, 0, 5));
     
 }
 
@@ -101,6 +87,7 @@ void FF_initGraph(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY]) {
  */
 void FF_addEdge(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY], int src, int dest, int weight) {
 	graph[src][dest] = weight;
+	capacity[src][dest] = weight;
 }
 
 
@@ -161,14 +148,17 @@ void FF_printGraph(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY]) {
 }
 
 
-
-
-
-int min (int x, int y) {
-    return x<y ? x : y;  // returns minimum of x and y
+/**
+ * Return min value
+ * Retorna o menor valor
+ *
+ * @author Luiz Venturote
+ */
+int min(int x, int y) {
+    return x<y ? x : y;
 }
 
-void enqueue (int x) {
+void enqueue(int x) {
     q[tail] = x;
     tail++;
     color[x] = GRAY;
@@ -182,73 +172,87 @@ int dequeue () {
 }
 
 
-int bfs (int start, int target) {
-    int u,v;
+int bfs(int flow[FF_VERTEX_QTY][FF_VERTEX_QTY], int start, int target) {
+	
+    int u,v, n=FF_VERTEX_QTY;
+    
     for (u=0; u<n; u++) {
-	color[u] = WHITE;
+		color[u] = WHITE;
     }   
+    
     head = tail = 0;
+    
     enqueue(start);
+    
     pred[start] = -1;
+    
     while (head!=tail) {
-	u = dequeue();
-        // Search all adjacent white nodes v. If the capacity
-        // from u to v in the residual network is positive,
-        // enqueue v.
-	for (v=0; v<n; v++) {
-	    if (color[v]==WHITE && capacity[u][v]-flow[u][v]>0) {
-		enqueue(v);
-		pred[v] = u;
-	    }
-	}
+    	
+		u = dequeue();
+		
+	    // Search all adjacent white nodes v. If the capacity
+	    // from u to v in the residual network is positive,
+	    // enqueue v.
+		for (v=0; v<n; v++) {
+		    if (color[v]==WHITE && capacity[u][v]-flow[u][v]>0) {
+				enqueue(v);
+				pred[v] = u;
+		    }
+		}
+		
     }
+    
     // If the color of the target node is black now,
     // it means that we reached it.
     return color[target]==BLACK;
 }
 
 
-int max_flow (int source, int sink) {
-    int i,j,u;
-    // Initialize empty flow.
-    int max_flow = 0;
-    for (i=0; i<n; i++) {
-	for (j=0; j<n; j++) {
-	    flow[i][j] = 0;
-	}
-    }
-    // While there exists an augmenting path,
-    // increment the flow along this path.
-    while (bfs(source,sink)) {
-        // Determine the amount by which we can increment the flow.
-	int increment = oo;
-	for (u=n-1; pred[u]>=0; u=pred[u]) {
-	    increment = min(increment,capacity[pred[u]][u]-flow[pred[u]][u]);
-	}
-        // Now increment the flow.
-	for (u=n-1; pred[u]>=0; u=pred[u]) {
-	    flow[pred[u]][u] += increment;
-	    flow[u][pred[u]] -= increment;
-	}
-	max_flow += increment;
-    }
-    // No augmenting path anymore. We are done.
-    return max_flow;
-}
-
-
-
-void read_input_file() {
+/**
+ * Ford Fulkerson algorithm
+ * @author Luiz Venturote
+ */
+int FordFulkerson(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY], int source, int sink) {
 	
-    int i,j;
+    int i,j,u, max_flow=0, n=FF_VERTEX_QTY, increment=0;
     
-    n=5;
+    // flow matrix
+    int flow[FF_VERTEX_QTY][FF_VERTEX_QTY];     
     
-    // initialize empty capacity matrix 
+    // Initialize empty flow
     for (i=0; i<n; i++) {
 		for (j=0; j<n; j++) {
-		    capacity[i][j] = rand() % 10;
+		    flow[i][j] = 0;
 		}
     }
     
+    // While there exists an augmenting path, increment the flow along this path
+    while( bfs(flow, source, sink) ) {
+    	
+        // Determine the amount by which we can increment the flow.
+		increment = FF_MAX_VALUE;
+		
+		for (u=n-1; pred[u]>=0; u=pred[u]) {
+			
+		    increment = min(increment, capacity[pred[u]][u]-flow[pred[u]][u]);
+		    
+		}
+	    
+		// Now increment the flow.
+		for(u=n-1; pred[u]>=0; u=pred[u]) {
+			
+		    flow[pred[u]][u] += increment;
+		    flow[u][pred[u]] -= increment;
+		    
+		}
+		
+		max_flow += increment;
+		
+    }
+    
+    // No augmenting path anymore. We are done.
+    return max_flow;
+    
 }
+
+
