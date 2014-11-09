eb22a0dabf0f6870578f554111f90130ec7b5a23
luizventurote <luiz_17@ymail.com>
2014-11-09 16:14:07 -0200

    Ford fulkerson algorithm finalized

--- maximum-flow/ford_fulkerson.h
+++ maximum-flow/ford_fulkerson.h
@@ -6,15 +6,18 @@
 // Valor máximo utilizado no algoritmo de Kruskal
 #define FF_MAX_VALUE 999
 
-#define WHITE 0
-#define GRAY 1
-#define BLACK 2
+// Define the maximum values of the markup table from queue in the BFS algorithm
+// Define os valores para gerênciar a tabela de marcação da fila no algoritmo BFS
+#define NEUTRAL 0
+#define MARKED 1
+#define UNMARKED 2
 
-int capacity[FF_VERTEX_QTY][FF_VERTEX_QTY]; // capacity matrix
-int color[FF_VERTEX_QTY]; // needed for breadth-first search               
-int pred[FF_VERTEX_QTY];  // array to store augmenting path
-int head,tail;
-int q[FF_VERTEX_QTY+2];
+// Variables for the algorithm queue (needed for breadth-first search)
+// Variáveis para o algoritmo de fila (Necessário para a busca em largura)
+int bfs_markup_table[FF_VERTEX_QTY];  
+int beginning_queue=0, end_queue=0;    
+int queue[FF_VERTEX_QTY+2];      
+int way[FF_VERTEX_QTY];
 
 
 /**
@@ -48,7 +51,7 @@ void FordFulkersonMF() {
 	
     // Print the result
     // Exibe o resultado
-    printf(" %d \n\n", FordFulkerson(graph, 0, 5));
+    printf(" Maximum flow is: %d \n\n", FordFulkerson(graph, 0, 5));
     
 }
 
@@ -69,7 +72,7 @@ void FF_initGraph(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY]) {
 		
 			// Add maximum value in all edges of the graph
 			// Adiciona o valor máximo em todos as arestas do grafo
-			graph[i][j] = FF_MAX_VALUE;
+			graph[i][j] = 0;
 		}
 	}
 }
@@ -87,7 +90,6 @@ void FF_initGraph(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY]) {
  */
 void FF_addEdge(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY], int src, int dest, int weight) {
 	graph[src][dest] = weight;
-	capacity[src][dest] = weight;
 }
 
 
@@ -158,101 +160,138 @@ int min(int x, int y) {
     return x<y ? x : y;
 }
 
+
+/**
+ * Enqueue
+ * Enfileirar
+ *
+ * @author Luiz Venturote
+ */
 void enqueue(int x) {
-    q[tail] = x;
-    tail++;
-    color[x] = GRAY;
+    queue[end_queue] = x;
+    end_queue++;
+    bfs_markup_table[x] = MARKED;
 }
 
-int dequeue () {
-    int x = q[head];
-    head++;
-    color[x] = BLACK;
+
+/**
+ * Dequeue
+ * Desenfileirar
+ *
+ * @author Luiz Venturote
+ */
+int dequeue() {
+    int x = queue[beginning_queue];
+    beginning_queue++;
+    bfs_markup_table[x] = UNMARKED;
     return x;
 }
 
 
-int bfs(int flow[FF_VERTEX_QTY][FF_VERTEX_QTY], int start, int target) {
+/**
+ * Breadth-First Search algorithm - BFS (https://www.youtube.com/watch?v=QRq6p9s8NVg)
+ *
+ * @author Luiz Venturote
+ * @param  int graph_capacity[][]
+ * @param  int flow[][]
+ * @param  int start  	Initial vertex
+ * @param  int target  	End vertex
+ * @return boolean 		If target vertex is UNMARKED return true
+ */
+int bfs(int graph_capacity[FF_VERTEX_QTY][FF_VERTEX_QTY], int flow[FF_VERTEX_QTY][FF_VERTEX_QTY], int start, int target) {
 	
-    int u,v, n=FF_VERTEX_QTY;
+    int i, u,v, n=FF_VERTEX_QTY;
     
-    for (u=0; u<n; u++) {
-		color[u] = WHITE;
+    // Initializes the markup table
+    // Inicializa a tabela de marcação
+    for(i=0; i<n; i++) {
+		bfs_markup_table[i] = NEUTRAL;
     }   
-    
-    head = tail = 0;
-    
+
+	// Adds the first vertex in the equeue
+	// Adiciona o primeiro vértice na fila
     enqueue(start);
     
-    pred[start] = -1;
+    // The first vertex should have no predecessor
+    // O primeiro vértice não deve ter antecessor
+    way[start] = -1;
     
-    while (head!=tail) {
+    while(beginning_queue!=end_queue) {
     	
+    	// Remove a vertex of the equeue
+    	// Retira um vértice da fila
 		u = dequeue();
 		
-	    // Search all adjacent white nodes v. If the capacity
-	    // from u to v in the residual network is positive,
-	    // enqueue v.
-		for (v=0; v<n; v++) {
-		    if (color[v]==WHITE && capacity[u][v]-flow[u][v]>0) {
+	    // Search all adjacent NEUTRAL nodes v. If the capacity from u to v in the network is positive, enqueue v.
+	    // Procura por todos os vértices adjacentes do vértice v. Se a capacidade de u para v na rede é positiva, enfilere v.
+		for(v=0; v<n; v++) {
+			
+		    if (bfs_markup_table[v]==NEUTRAL && graph_capacity[u][v]-flow[u][v]>0) {
+				
 				enqueue(v);
-				pred[v] = u;
+				way[v] = u;
+				
 		    }
+		    
 		}
-		
     }
     
-    // If the color of the target node is black now,
-    // it means that we reached it.
-    return color[target]==BLACK;
+    // If the markup table of the target node is UNMARKED now, it means that we reached it.
+    // Se o vértice alvo estiver como UNMARKED, significa que o algoritmo conseguiu encontra-lo
+    return bfs_markup_table[target] == UNMARKED;
 }
 
 
 /**
  * Ford Fulkerson algorithm
+ *
  * @author Luiz Venturote
+ * @param  int graph_capacity[][]
+ * @param  int start  	Initial vertex
+ * @param  int target  	End vertex
  */
-int FordFulkerson(int graph[FF_VERTEX_QTY][FF_VERTEX_QTY], int source, int sink) {
+int FordFulkerson(int graph_capacity[FF_VERTEX_QTY][FF_VERTEX_QTY], int start, int target) {
 	
     int i,j,u, max_flow=0, n=FF_VERTEX_QTY, increment=0;
     
-    // flow matrix
+    // Flow matrix
+    // Matriz de fluxo
     int flow[FF_VERTEX_QTY][FF_VERTEX_QTY];     
     
     // Initialize empty flow
+    // Inicializa a matriz de fluxo
     for (i=0; i<n; i++) {
 		for (j=0; j<n; j++) {
 		    flow[i][j] = 0;
 		}
     }
     
-    // While there exists an augmenting path, increment the flow along this path
-    while( bfs(flow, source, sink) ) {
+    // Busca em largura. Enquanto existir um caminho de alcance, incrementa o fluxo ao londo desse caminho
+    // BFS breadth first search. While there exists an augmenting path, increment the flow along this path
+    while( bfs(graph_capacity, flow, start, target) ) {
     	
-        // Determine the amount by which we can increment the flow.
 		increment = FF_MAX_VALUE;
 		
-		for (u=n-1; pred[u]>=0; u=pred[u]) {
-			
-		    increment = min(increment, capacity[pred[u]][u]-flow[pred[u]][u]);
-		    
+		// Find the shortest path with capacity for flow
+		// Encontra o caminho mínimo com capacidade para o fluxo
+		for(u=n-1; way[u]>=0; u=way[u]) {
+		    increment = min(increment, graph_capacity[way[u]][u]-flow[way[u]][u]);
 		}
 	    
-		// Now increment the flow.
-		for(u=n-1; pred[u]>=0; u=pred[u]) {
-			
-		    flow[pred[u]][u] += increment;
-		    flow[u][pred[u]] -= increment;
-		    
+		// Increment the flow
+		// Incrementa o fluxo
+		for(u=n-1; way[u]>=0; u=way[u]) {
+		    flow[way[u]][u] += increment;
+		    flow[u][way[u]] -= increment;
 		}
 		
+		// Increment thw maximum flow
+		// Incrementa o fluxo máximo
 		max_flow += increment;
 		
     }
     
-    // No augmenting path anymore. We are done.
+    // When no path is free, the algorithm returns the result of the maximum flow 
+    // Quando nenhum caminho estiver livre, o algoritmo retorna o resultado do fluxo máximo
     return max_flow;
-    
 }
-
-
